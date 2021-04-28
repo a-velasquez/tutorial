9ec4f845845f43c928168d13895e45d71fc47f3e
Alex Velazquez <avelasquez075@gmail.com>
2021-04-28 15:13:01 -0700

    cleanup code, add inline and reference function to eventHandlers

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const books = [
     id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/51Ix49rxgtL._SY414_BO1,204,203,200_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth",
+    title: "Goodnight Moon",
+    author: "Margaret Wise Brown",
   },
   {
     id: 3,
@@ -27,48 +27,14 @@ const books = [
 ];
 
 
-
-
 // stateless functional component
 function BookList() {
   // always return JSX
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { id, img, title, author } = book;
-        return (
-          <Book key={book.id} book={book}></Book>
-        );
+        return <Book key={book.id} {...book}></Book>;
       })}
-      {/* <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Sit id elit ea aliqua aliqua consectetur occaecat adipisicing velit.
-          Lorem ad qui irure quis amet labore adipisicing dolore mollit.
-          Incididunt voluptate commodo nostrud laboris id ad id incididunt ut
-          irure do occaecat ullamco laboris. Consectetur fugiat aliqua eiusmod
-          voluptate adipisicing consectetur reprehenderit veniam consequat
-          consectetur id ex.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        <p>
-          Veniam quis veniam esse culpa deserunt magna. Sit do excepteur culpa
-          labore consequat. Elit irure aute et eiusmod deserunt Lorem nostrud
-          nisi ullamco nulla. Mollit elit magna ex eiusmod nulla minim officia
-          veniam. Eiusmod Lorem fugiat exercitation esse Lorem commodo mollit eu
-          culpa ullamco eu do. Ipsum elit officia esse deserunt reprehenderit
-          fugiat exercitation non ad amet. Labore exercitation tempor dolor
-          nulla cillum minim cupidatat dolore qui commodo Lorem commodo.
-        </p>
-      </Book> */}
     </section>
   );
 }
@@ -78,16 +44,25 @@ function BookList() {
 // const Book = ({ img, title, author, children }) {
 
 const Book = (props) => {
+// atribute, eventHandler
+// onClick, onMouseOver
+  
 // method 2 - as VAR
-  const { img, title, author, children } = props.book;
+  const { img, title, author } = props;
+
+  const clickHandler = () => {
+    alert('Hello World')
+  }
 
   console.log(props)
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h2>{title}</h2>
+      {/* inline function */}
+      <h2 onClick={() => console.log(title)}>{title}</h2>
       <h4>{author}</h4>
-      {children}
+      {/* reference to function */}
+      <button onClick={clickHandler}>Reference Example</button>
     </article>  
   );
 }
