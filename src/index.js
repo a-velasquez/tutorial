b61d40ff96fd530d0453a218e21ff77d6f70f732
Alex Velazquez <avelasquez075@gmail.com>
2021-04-28 16:59:29 -0700

    import {book} and Book components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import './index.css'
 // .js not needed because ./books.js is a js file
-import { books } from './books'
-import { Book } from './Book'
+import { books } from './books';
+import Book from './Book';
 
 // stateless functional component
 function BookList() {
