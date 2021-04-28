202f0cd1ed893de471cfbe24617cf8e97b7656ec
Alex Velazquez <avelasquez075@gmail.com>
2021-04-28 16:59:49 -0700

    move books to books.js

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51Ix49rxgtL._SY414_BO1,204,203,200_.jpg",
+    title: "Goodnight Moon",
+    author: "Margaret Wise Brown",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/41ThX3SfAiL.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "Brit Bennett",
+  },
+];
